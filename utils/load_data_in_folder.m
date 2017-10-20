4af5350efd3347bf3b461bc48583ff1ee08efbb8
Rendell Cale <rendellc@stud.ntnu.no>
2017-10-20 22:48:25 +0200

    add utils for loading data to matlab

---
+++ utils/load_data_in_folder.m
@@ -0,0 +1,18 @@
+toppath = genpath(pwd);
+addpath(toppath);
+
+pushd('../data - complete/Part 4/polefactor_5/integral');
+
+
+files = dir();
+for i=3:(size(files,1) - 1) 
+    if files(i).isdir == 0 && strcmp(files(i).name((end-2):end), "mat") 
+        load(files(i).name)
+    end
+end
+
+popd
+
+rmpath(toppath);
+
+
