d1c3f1108ec3e27fd4b9871c9bc19f6625f019cf
Rendell Cale <rendellc@stud.ntnu.no>
2017-11-03 19:08:32 +0100

    Add boatfiles p1 to p3

---
+++ boat/code/p5p3.m
@@ -0,0 +1,23 @@
+% from problem 1
+K = 0.1397; 
+T = 70.2393;
+Hship = tf([K], [T, 1, 0]);
+
+% reference
+psi_r = 30;
+
+
+% PD-reg
+Td = T;
+wc = 0.1;
+phi = 50; % deg
+Tf = -1/(tan(deg2rad(180-phi))*wc);
+Kpd = 1;
+Hpd = tf([Kpd*Td, Kpd], [Tf, 1]);
+
+bode(Hpd*Hship);
+
+load('../../data/p3b.mat');
+ts=ans;
+
+
